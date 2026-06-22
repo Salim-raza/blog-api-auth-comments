14e67d070c25a3f37bab46bda00b1de5c90c40b0
Salim Raza <rezasalim00011@gmail.com>
2026-06-22 15:58:04 +0600

    Optimize whitespace in serializers.py
    Removed unnecessary blank lines to enhance code readability.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,22 +2,18 @@ from rest_framework import serializers
 from .models import *
 
 
-
 class UserCreateSerializers(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ["fullname", "email", "password", "phone", "create_at", "update_at"]
         
-        
 class SigninSerializers(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
     
-    
 class ChangePasswordSerializers(serializers.Serializer):
     old_password = serializers.CharField()
     new_password = serializers.CharField()
-    
     
 class CreateOtpSerializers(serializers.Serializer):
     email = serializers.CharField()
