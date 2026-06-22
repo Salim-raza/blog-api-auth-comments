ac6daaa10bb3fcede506909112a8db4ec329d272
Salim Raza <rezasalim00011@gmail.com>
2026-06-22 23:25:45 +0600

    Add author field to Category model

--- blogpost/models.py
+++ blogpost/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=True, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
