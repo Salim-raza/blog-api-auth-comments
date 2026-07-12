98e337408566f0c71ecf4b9738a0b56f7666c89c
Salim Raza <rezasalim00011@gmail.com>
2026-07-12 21:16:32 +0600

    Reorganize import statements in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.shortcuts import get_object_or_404
-from rest_framework.authentication import authenticate
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import authenticate
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
-import random
 from .utils import *
+import random
 
 
 
